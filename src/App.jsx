6caede3d2f9389870462ff07183f9a38cbd3e327
Estefania <estefaniaromero.torrents@gmail.com>
2023-01-10 17:26:09 +0100

    [Added] first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import AlertButton from "./components/AlertButton";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/app">App</a>
+        <a href="/app">App</a>
+      </header>
+      <AlertButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AlertButton.jsx
@@ -0,0 +1,11 @@
+// AlertButton.js
+function AlertButton() {
+  return (
+    <div>
+      <button onClick={() => alert("Alert Button Clicked!")}>
+        Alert Button
+      </button>
+    </div>
+  );
+}
+export default AlertButton;
